4336f190a29d1be83446a4b7d971ba843a9ebacc
Steddi <steddi.andritiana@pulse.mg>
2024-03-14 12:13:45 +0300

    first commit

---
+++ src/main/java/pulse/gps/GpsApplication.java
@@ -0,0 +1,13 @@
+package pulse.gps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pulse/gps/GpsApplicationTests.java
@@ -0,0 +1,13 @@
+package pulse.gps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
